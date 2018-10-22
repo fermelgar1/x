58cb1a5fce13ceb94e2ad642a3db66fe75d42b36
fermelgar1 <39498355+fermelgar1@users.noreply.github.com>
2018-10-21 22:29:01 -0500

    basiado

--- info.php
+++ info.php
@@ -6,143 +6,282 @@
 </div>
 <br>
 <div class="row">
-<div class="container">
+    <!--Ambiental-->
     <div class="container">
-        <br>
-        <h5>ACTIVIDADES DE INGENIERIA AMBIENTAL</h5>
-        <br>
+        <div class="container">
+            <br>
+            <h5>ACTIVIDADES DE INGENIERIA AMBIENTAL</h5>
+            <br>
+        </div>
+        <div class="container">
+            <div class="container">
+                <h6>Conferencias</h6>
+            </div>
+            <div class="container">
+                <p>
+                    manejo de residuos porcicola <br>
+                    Mtro. Gerardo Dominguez araujo <br>
+                    25 de octubre 16:30 a 18:00
+                </p>
+                <p>
+                    Selección de sitios para implementacion de relleno sanitario <br>
+                    Ing. Alfredo García Pérez <br>
+                    25 de octubre 18:00 a 19:30
+                </p>
+            </div>
+            <br>
+            <div class="container">
+                <h6>Taller</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Elaboración de Atlas de Riesgo <br>
+                    Ing. Alfredo García Pérez <br>
+                    26 de octubre 15:00 a 18:00
+                </p>
+            </div>
+        </div>
     </div>
-    
+    <!--gem-->
     <div class="container">
+        <div class="container">
+            <br>
+            <h5>ACTIVIDADES DE INGENIERIA EN GERSTION EMPRESARIAL</h5>
+            <br>
+        </div>
+        <div class="container">
+            <div class="container">
+                <h6>Conferencias</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Prácticas Desleales del Comercio Internacional <br>
+                    Dr. Eduardo Castañeda Chávez <br>
+                    25 de Octubre 9:00 am
+                </p>
+                <p>
+                    Como Hacer Negocios con China. <br>
+                    Dr. Eduardo Castañeda Chávez <br>
+                    25 de Octubre 2:30 pm
+                </p>
+                <p>
+                    Vigilancia tecnológica, herramienta para la toma de decisiones de una organización. <br>
+                    Alejandra Rosales Soto <br>
+                    26 de Octubre 9:00 am
+                </p>
+                <p>
+                    Desarrollo de Negocios <br>
+                    Co - propietario de  Tejas el Águila  <br>
+                    26 de Octubre 11:00 am
+                </p>
+                <p>
+                    Las Etiquetas y certificaciones ambientales en la industria, que son y para qué sirven <br> 
+                    Dr. Isela López López <br>
+                    26 de Octubre 3:00 pm
+                </p>
+                <p>
+                    Marcas Con Valor. <br>
+                    Mtro. Francisco Cervantes García <br>
+                    26 de Octubre 5:00 pm <br>
+                </p>
+            </div>
+            <div class="container">
+                <h6>Talleres</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Identificación  de invenciones y características  a proteger<br>
+                    IMPI <br>
+                    25 de Octubre 4:15 pm
+                </p>
+                <p>
+                    Como clasificar  adecuadamente  tu marca  y/o llenado de  solicitudes  y signos distintivos <br>
+                    IMPI <br>
+                    25 de Octubre 11:00 am
+                </p>
+            </div>
+        </div>
+    </div>
+     <!--electro-->
     <div class="container">
-        <h6>Conferencias</h6>
+        <div class="container">
+            <br>
+            <h5 class="">ACTIVIDADES DE INGENIERÍA ELECTROMECÁNICA</h5>
+            <br>
+        </div>
+        <div class="container">
+            <div class="container">
+                <h6>Conferencias</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Cálculo y selección de bombas <br>
+                    Ing. Fernando González Torres <br>
+                    25 de Octubre 4:00 – 6:00 p.m.
+                </p>
+                <p>
+                    El papel del ingeniero en el área eléctrica <br>
+                    Ing. Jesús Arturo Espinoza Villa <br>
+                    25 de Octubre 10:00 – 12:00 p.m.
+                </p>
+                <p>
+                    Redes de distribución eléctrica en media tensión <br>
+                    Ing. Juan Pablo Hernández Jiménez <br>
+                    26 de Octubre 4:00-6:00 p.m. 
+                </p>
+            </div>
+            <div class="container">
+                <h6>Taller</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Manejo de incendios<br>
+                    Ing. Julio César Marín Amezquita <br>
+                    26 de Octubre 10:00 – 12:00 p.m.
+                </p>
+            </div>
+        </div>
     </div>
+    <!--alimentos-->
     <div class="container">
-        <p>
-            manejo de residuos porcicola <br>
-            Mtro. Gerardo Dominguez araujo <br>
-            25 de octubre 16:30 a 18:00
-        </p>
-        <p>
-            Selección de sitios para implementacion de relleno sanitario <br>
-            Ing. Alfredo García Pérez <br>
-            25 de octubre 18:00 a 19:30
-        </p>
+        <div class="container">
+            <br>
+            <h5 class="">ACTIVIDADES DE INGENIERÍA EN INDUSTRIAS ALIMENTARIAS</h5>
+            <br>    
+        </div>  
+        <div class="container">
+            <div class="container">
+                <h6>Conferencias</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Presencia de microToxinas <br>
+                    Erika guadalupe<br>
+                    taller de alimentos edificio b <br>
+                    25 agosoto 5 pm
+                </p>
+                <p>
+                    parametro de calidad de los sistemas de produccion carnicos <br>
+                    Ing. Francisco Javier Araujo <br>
+                    Aula de usuos multiples <br>
+                    24 agosoto 1 pm
+                </p>
+            </div>
+            <div class="container">
+                <h6>Taller</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Elaboracion de polvitos magicos <br>
+                    Ing. Carlos Omar Garcia <br>
+                    taller de alimentos edificio b <br>
+                    22 agosoto 11 am
+                </p>
+                <p>
+                    Elaboracion de ate de guayaba <br>
+                    Ing. Agustin Jaime Delgadillo  <br>
+                    taller de alimentos edificio b <br>
+                    22 agosoto 5 pm
+                </p>
+                <p>
+                    Elaboracion de cacahuate Japones <br>
+                    Ing. Lorenzo Zamorano <br>
+                    taller de alimentos edificio b <br>
+                    23 agosoto 11 am
+                </p>
+                <p>
+                    Elaboracion de polvitos magicos <br>
+                    Ing. Rosendo Velazquez <br>
+                    taller de alimentos edificio b <br>
+                    23 agosoto 5 pm
+                </p>
+                <p>
+                    Elaboracion de polvitos magicos <br>
+                    Ing. Carlos Omar Garcia <br>
+                    taller de alimentos edificio b <br>
+                    23 agosoto 5 pm
+                </p>
+                <p>
+                    Elaboracion de cacahuate Japones <br>
+                    Ing. Carlos Omar Garcia <br>
+                    taller de alimentos edificio b <br>
+                    24 agosoto 11 am
+                </p>
+                <p>
+                    Elaboracion de Helado <br>
+                    Ing. Maria Lopez e Ing. Carlos Omar Garcia <br>
+                    taller de alimentos edificio b <br>
+                    24 agosoto 5 pm
+                </p>
+                <p>
+                    parametros de calidad en la elaboracion de queso panela <br>
+                    Ing. Oscar Ornelas Martin e Ing. Carlos Omar Garcia <br>
+                    taller de alimentos edificio b <br>
+                    25 agosoto 1 pm
+                </p>
+                <p>
+                    Elaboracion de productos a base de suero de leche<br>
+                    Ing. Rosendo Velazquez e Isaac Contreras <br>
+                    taller de alimentos edificio b <br>
+                    25 agosoto 1 pm
+                </p>
+            </div>
+        </div>
     </div>
-    <br>
+    <!--SISTEMAS-->
     <div class="container">
-        <h6>Taller</h6>
+        <div class="container">
+            <br>
+            <h5 class="">ACTIVIDADES DE INGENIERÍA EN SISTEMAS COMPUTACIONALES</h5>
+            <br>
+        </div>
+        <div class="container">
+            <div class="container">
+                <h6>Conferencias</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Emprendurismo (con enfoque en TI) <br>
+                    Alejandro Chávez Castillo  <br>
+                    25 de Octubre 11 – 12:00 p.m.
+                </p>
+                <p>
+                    Administración de proyectos en TIs <br>
+                    Alejandro Chávez Castillo  <br>
+                    26 de Octubre 12:00 – 1:00 p.m.
+                </p>
+            </div>
+            <div class="container">
+                <h6>Taller</h6>
+            </div>
+            <div class="container">
+                <p>
+                    Desarrollo de aplicaciones móviles con Android Studio y SqLite<br>
+                    Juana Cecilia Dávila Barajas <br>
+                    26 de Octubre 8:00 - 14:00
+                </p>
+                <p>
+                    Backend with Node and Express<br>
+                    Miguel Ángel Jaime López <br>
+                    26 de Octubre 10:00-14:00
+                </p>
+                <p>
+                    Programación en Javascript<br>
+                    Jonathan Gonzalez Franco <br>
+                    26 de Octubre 8:00 - 14:00
+                </p>
+            </div>
+        </div>
     </div>
-    <div class="container">
-        <p>
-            Elaboración de Atlas de Riesgo <br>
-            Ing. Alfredo García Pérez <br>
-            26 de octubre 15:00 a 18:00
-        </p>
-    </div>
-    </div>
-    </div>
-    <!--carreras-->
-    <div class="container">
-    <div class="container">
-        <br>
-        <h5>ACTIVIDADES DE INGENIERIA EN GERSTION EMPRESARIAL</h5>
-        <br>
-    </div>
-    
-    <div class="container">
-    <div class="container">
-        <h6>Conferencias</h6>
-    </div>
-    <div class="container">
-        <p>
-            Prácticas Desleales del Comercio Internacional <br>
-            Dr. Eduardo Castañeda Chávez <br>
-            25 de Octubre 9:00 am
-        </p>
-        <p>
-            Como Hacer Negocios con China. <br>
-            Dr. Eduardo Castañeda Chávez <br>
-            25 de Octubre 2:30 pm
-        </p>
-        <p>
-            Vigilancia tecnológica, herramienta para la toma de decisiones de una organización. <br>
-            Alejandra Rosales Soto <br>
-            26 de Octubre 9:00 am
-        </p>
-        <p>
-            Desarrollo de Negocios <br>
-            Co - propietario de  Tejas el Águila  <br>
-            26 de Octubre 11:00 am
-        </p>
-        <p>
-            Las Etiquetas y certificaciones ambientales en la industria, que son y para qué sirven <br>
-            Dr. Isela López López <br>
-            26 de Octubre 3:00 pm
-        </p>
-        <p>
-        Marcas Con Valor. <br>
-        Mtro. Francisco Cervantes García <br>
-        26 de Octubre 5:00 pm <br>
-        </p>
-    </div>
-    <div class="container">
-        <h6>Talleres</h6>
-    </div>
-    <div class="container">
-        <p>
-            Identificación  de invenciones y características  a proteger<br>
-            IMPI <br>
-            25 de Octubre 4:15 pm
-        </p>
-        <p>
-            Como clasificar  adecuadamente  tu marca  y/o llenado de  solicitudes  y signos distintivos <br>
-            IMPI <br>
-            25 de Octubre 11:00 am
-        </p>
-    </div>
-    </div>
-    </div>
-     <!--carreras-->
-     <div class="container">
-     <div class="container">
-        <br>
-        <h5 class="">ACTIVIDADES DE INGENIERÍA ELECTROMECÁNICA</h5>
-        <br>
-    </div>
-    
-    <div class="container">
-    <div class="container">
-        <h6>Conferencias</h6>
-    </div>
-    <div class="container">
-        <p>
-            Cálculo y selección de bombas <br>
-            Ing. Fernando González Torres <br>
-            25 de Octubre 4:00 – 6:00 p.m.
-        </p>
-        <p>
-            El papel del ingeniero en el área eléctrica <br>
-            Ing. Jesús Arturo Espinoza Villa <br>
-            25 de Octubre 10:00 – 12:00 p.m.
-        </p>
-        <p>
-            Redes de distribución eléctrica en media tensión <br>
-            Ing. Juan Pablo Hernández Jiménez <br>
-            26 de Octubre 4:00-6:00 p.m. 
-        </p>
-    </div>
-    <div class="container">
-        <h6>Taller</h6>
-    </div>
-    <div class="container">
-        <p>
-            Manejo de incendios<br>
-            Ing. Julio César Marín Amezquita <br>
-            26 de Octubre 10:00 – 12:00 p.m.
-        </p>
-    </div>
-    </div>
-    </div>
-
 </div>
+<style>
+    p{
+        font-family: inherit;
+    }
+    h6{
+        font-family: inherit;
+    }
+    h5{
+        font-family: inherit;
+    }
+</style>
