a89059e7efa9dca03bf55e78d49fbd7c345e8c14
fermelgar1 <39498355+fermelgar1@users.noreply.github.com>
2018-10-21 20:57:34 -0500

    kk

--- info.php
+++ info.php
@@ -4,11 +4,16 @@
         <h3>Tecnologico Mario Molina</h3>
     </div>
 </div>
-
+<br>
 <div class="row">
+<div class="container">
     <div class="container">
+        <br>
         <h5>ACTIVIDADES DE INGENIERIA AMBIENTAL</h5>
+        <br>
     </div>
+    
+    <div class="container">
     <div class="container">
         <h6>Conferencias</h6>
     </div>
@@ -24,6 +29,7 @@
             25 de octubre 18:00 a 19:30
         </p>
     </div>
+    <br>
     <div class="container">
         <h6>Taller</h6>
     </div>
@@ -34,10 +40,17 @@
             26 de octubre 15:00 a 18:00
         </p>
     </div>
+    </div>
+    </div>
     <!--carreras-->
     <div class="container">
+    <div class="container">
+        <br>
         <h5>ACTIVIDADES DE INGENIERIA EN GERSTION EMPRESARIAL</h5>
+        <br>
     </div>
+    
+    <div class="container">
     <div class="container">
         <h6>Conferencias</h6>
     </div>
@@ -48,36 +61,88 @@
             25 de Octubre 9:00 am
         </p>
         <p>
-            Taller  del IMPI: Como clasificar  adecuadamente  tu marca  y/o llenado de  solicitudes  y signos distintivos <br>
-            IMPI <br>
-            25 de Octubre 11:00 am
-        </p>
-        <p>
             Como Hacer Negocios con China. <br>
             Dr. Eduardo Castañeda Chávez <br>
             25 de Octubre 2:30 pm
-        </p>
-        <p>
-            Taller del IMPI: Identificación  de invenciones y características  a proteger<br>
-            IMPI <br>
-            25 de Octubre 4:15 pm
         </p>
         <p>
             Vigilancia tecnológica, herramienta para la toma de decisiones de una organización. <br>
             Alejandra Rosales Soto <br>
             26 de Octubre 9:00 am
         </p>
+        <p>
+            Desarrollo de Negocios <br>
+            Co - propietario de  Tejas el Águila  <br>
+            26 de Octubre 11:00 am
+        </p>
+        <p>
+            Las Etiquetas y certificaciones ambientales en la industria, que son y para qué sirven <br>
+            Dr. Isela López López <br>
+            26 de Octubre 3:00 pm
+        </p>
+        <p>
+        Marcas Con Valor. <br>
+        Mtro. Francisco Cervantes García <br>
+        26 de Octubre 5:00 pm <br>
+        </p>
     </div>
-    <!--talleres    
+    <div class="container">
+        <h6>Talleres</h6>
+    </div>
+    <div class="container">
+        <p>
+            Identificación  de invenciones y características  a proteger<br>
+            IMPI <br>
+            25 de Octubre 4:15 pm
+        </p>
+        <p>
+            Como clasificar  adecuadamente  tu marca  y/o llenado de  solicitudes  y signos distintivos <br>
+            IMPI <br>
+            25 de Octubre 11:00 am
+        </p>
+    </div>
+    </div>
+    </div>
+     <!--carreras-->
+     <div class="container">
+     <div class="container">
+        <br>
+        <h5 class="">ACTIVIDADES DE INGENIERÍA ELECTROMECÁNICA</h5>
+        <br>
+    </div>
+    
+    <div class="container">
+    <div class="container">
+        <h6>Conferencias</h6>
+    </div>
+    <div class="container">
+        <p>
+            Cálculo y selección de bombas <br>
+            Ing. Fernando González Torres <br>
+            25 de Octubre 4:00 – 6:00 p.m.
+        </p>
+        <p>
+            El papel del ingeniero en el área eléctrica <br>
+            Ing. Jesús Arturo Espinoza Villa <br>
+            25 de Octubre 10:00 – 12:00 p.m.
+        </p>
+        <p>
+            Redes de distribución eléctrica en media tensión <br>
+            Ing. Juan Pablo Hernández Jiménez <br>
+            26 de Octubre 4:00-6:00 p.m. 
+        </p>
+    </div>
     <div class="container">
         <h6>Taller</h6>
     </div>
     <div class="container">
         <p>
-            Elaboración de Atlas de Riesgo <br>
-            Ing. Alfredo García Pérez <br>
-            26 de octubre 15:00 a 18:00
+            Manejo de incendios<br>
+            Ing. Julio César Marín Amezquita <br>
+            26 de Octubre 10:00 – 12:00 p.m.
         </p>
     </div>
-    -->
+    </div>
+    </div>
+
 </div>
